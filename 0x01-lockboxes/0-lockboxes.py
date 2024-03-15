1fa6818eb1d2c607903259ad93c67c7316cdddce
GeebeeFrost <omgbolahan1@gmail.com>
2024-03-15 11:15:28 +0100

    Add Python function to determine if boxes can be opened

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+"""This script contains the canUnlockAll function"""
+
+
+def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened."""
+    opened_boxes = set()
+    opened_boxes.add(0)
+    key_stack = [0]
+
+    while key_stack:
+        box = key_stack.pop()
+
+        for key in boxes[box]:
+            if key < len(boxes) and key not in opened_boxes:
+                key_stack.append(key)
+                opened_boxes.add(key)
+
+    return len(opened_boxes) == len(boxes)
