5a79df3eeaeb0809d8b772ff0c63dd9bf55d2503
GeebeeFrost <omgbolahan1@gmail.com>
2024-04-01 16:23:02 +0100

    Add Python script to read stdin log

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This script reads stdin line by line and computes metrics."""
+import sys
+import re
+
+codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+
+pattern = r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[([^\]]+)\] \"GET /projects/260 HTTP/1.1\" (\d{3}) (\d+)"
+
+
+if __name__ == "__main__":
+    count = 0
+    file_size = 0
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                file_size += int(data[-1])
+            except Exception:
+                pass
+            try:
+                if int(data[-2]) in codes:
+                    codes[int(data[-2])] += 1
+            except Exception:
+                pass
+            if count % 10 == 0:
+                print("File size: {}".format(file_size))
+                for key, value in sorted(codes.items()):
+                    if value != 0:
+                        print("{}: {}".format(key, value))
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("File size: {}".format(file_size))
+        for key, value in sorted(codes.items()):
+            if value != 0:
+                print("{}: {}".format(key, value))
