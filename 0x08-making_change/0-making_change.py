0261365f87af333d65e01ac65e1c2261d85b029e
GeebeeFrost <omgbolahan1@gmail.com>
2024-05-01 18:09:29 +0100

    Add Python function to determine the fewest number of coins needed to meet a given total

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This script contains the makeChange function."""
+
+
+def makeChange(coins, total):
+    """
+    This function makes change for a given total using the fewest coins.
+
+    Args:
+        coins: list of the values of the coins available.
+        total: the total to make change for.
+
+    Returns:
+        The fewest number of coins needed to meet total.
+        If the total cannot be made with the coins available, return -1.
+    """
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    num_coins = 0
+    for coin in coins:
+        if total <= 0:
+            break
+        num_coins += total // coin
+        total = total % coin
+    if total != 0:
+        return -1
+    return num_coins
+
+
+print(makeChange([1, 2, 25], 37))
+
+print(makeChange([1256, 54, 48, 16, 102], 1453))
